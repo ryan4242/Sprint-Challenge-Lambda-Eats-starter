eeeb4fb62b8b487fab9245dbe58544653b59f3d7
Ryan McInnis <ryan.mcinnis42@gmail.com>
2020-06-12 20:57:53 -0700

    fixed bug so total resets when you navigate home

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
 
   return (
     <div>
-      <Nav />
+      <Nav setTotal={setTotal} />
       
       <Switch>
         <Route path='/pizza'>
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -3,11 +3,15 @@ import {Link} from 'react-router-dom';
 import './nav.css';
 
 const Nav = props => {
+  const handleClick = e => {
+    props.setTotal([])
+  }
+
   return (
     <div className='nav-container'>
       <h1>Lambda Eats</h1>
       <nav>
-        <Link className='link' to='/' >Home</Link>
+        <Link className='link' to='/' onClick={handleClick} >Home</Link>
       </nav>
     </div>
   )
